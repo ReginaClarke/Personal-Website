cca31bab054ea81195c718a747da6daa726bbaea
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-04-22 16:28:58 -0400

    pends screensharing but worked through responsiveness

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,15 @@
 import React from "react";
 
+const resume = function () {
+  window.open(
+    "https://res.cloudinary.com/dolrvzlkq/image/upload/v1587571377/Portfolio/Resume-Regina_Clarke_dgqtns.pdf",
+    "_blank"
+  );
+};
+
 function Resume() {
   return (
     <div className="resumeimage">
-      {/* <button className="gettoknowme" onClick={resume}>
-        download
-      </button> */}
-
       <div></div>
       <iframe
         title="Resume"
@@ -15,6 +18,11 @@ function Resume() {
         src="https://res.cloudinary.com/dolrvzlkq/image/upload/v1587571377/Portfolio/Resume-Regina_Clarke_dgqtns.pdf"
         alt="resume"
       />
+      <div>
+      <button className="gettoknowme" onClick={resume}>
+          Additional Way to Download
+      </button>
+      </div>
     </div>
   );
 }
