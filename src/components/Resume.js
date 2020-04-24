e9640bc20547b9f32d1e875a8709ffcd458c7e44
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-04-24 00:31:06 -0400

    adjusted resume sizing for netlify

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,17 +10,19 @@ const resume = function () {
 function Resume() {
   return (
     <div className="resumeimage">
-     
       <iframe
         title="Resume"
+        type="application/pdf"
+        width="100%"
+        height="100%"
         className="resume"
-        src="https://res.cloudinary.com/dolrvzlkq/image/upload/v1587571377/Portfolio/Resume-Regina_Clarke_dgqtns.pdf#zoom=FitW"
+        src="https://res.cloudinary.com/dolrvzlkq/image/upload/v1587571377/Portfolio/Resume-Regina_Clarke_dgqtns.pdf"
         alt="resume"
       />
       <div>
-      <button className="gettoknowme" onClick={resume}>
+        <button className="gettoknowme" onClick={resume}>
           Additional Way to Download
-      </button>
+        </button>
       </div>
     </div>
   );
